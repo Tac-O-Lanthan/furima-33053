3afb5160834ff58cb6135bf11e33428e20f6d57a
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-01-18 17:31:45 +0900

    商品出品機能の実装（修正1）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,38 +9,6 @@ RSpec.describe Item, type: :model do
       it 'すべての値が必須であること' do
         expect(@item).to be_valid
       end
-      it 'カテゴリーの情報が必須であること' do
-        @item.category_id = '2'
-        expect(@item).to be_valid
-      end
-      it '商品の状態についての情報が必須であること' do
-        @item.sales_status_id = '2'
-        expect(@item).to be_valid
-      end
-      it '配送料の負担についての情報が必須であること' do
-        @item.shipping_fee_status_id = '2'
-        expect(@item).to be_valid
-      end
-      it '発送元の地域について情報が必須であること' do
-        @item.prefecture_id = '2'
-        expect(@item).to be_valid
-      end
-      it '発送までの日数についての情報が必須であること' do
-        @item.scheduled_delivery_id = '2'
-        expect(@item).to be_valid
-      end
-      it '価格の範囲が、¥300~¥9,999,999の間であること（下限）' do
-        @item.category_id = '300'
-        expect(@item).to be_valid
-      end
-      it '価格の範囲が、¥300~¥9,999,999の間であること（上限）' do
-        @item.category_id = '9999999'
-        expect(@item).to be_valid
-      end
-      it '販売価格は、半角数字のみ保存可能であること' do
-        @item.category_id = '300'
-        expect(@item).to be_valid
-      end
     end
     context '異常系' do
       it '商品名が必須であること' do
@@ -59,27 +27,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Info can't be blank"
       end
       it 'カテゴリーの情報が必須であること' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include 'Category must be other than 1'
       end
       it '商品の状態についての情報が必須であること' do
-        @item.sales_status_id = '1'
+        @item.sales_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include 'Sales status must be other than 1'
       end
       it '配送料の負担についての情報が必須であること' do
-        @item.shipping_fee_status_id = '1'
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include 'Shipping fee status must be other than 1'
       end
       it '発送元の地域について情報が必須であること' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include 'Prefecture must be other than 1'
       end
       it '発送までの日数についての情報が必須であること' do
-        @item.scheduled_delivery_id = '1'
+        @item.scheduled_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include 'Scheduled delivery must be other than 1'
       end
